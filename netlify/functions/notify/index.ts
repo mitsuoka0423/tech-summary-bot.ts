4f7738861dc72b37ad5eeb38c032c31428168e86
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-04-02 11:13:35 +0900

    fix(インターフェース変更): インタフェース変更

--- netlify/functions/notify/index.ts
+++ netlify/functions/notify/index.ts
@@ -1,11 +1,11 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import { main } from "../../../src/application/service/qiitaSummary";
+import { execute } from "../../../src/application/service/qiitaSummary";
 
 const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
-  await main();  
+  await execute();  
 
   return {
     statusCode: 200,
