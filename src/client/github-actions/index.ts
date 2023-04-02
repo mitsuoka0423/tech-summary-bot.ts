023d5dd806eae7bd2ddf8e957f0039dbc747b152
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-04-02 12:00:01 +0900

    feat: add esbuild

---
+++ src/client/github-actions/index.ts
@@ -0,0 +1,11 @@
+import { adaptor } from "../../application/adaptor/client-adaptor/driver-adaptor";
+
+import { config } from "dotenv";
+config();
+
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || "";
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY || "";
+
+adaptor({ DISCORD_WEBHOOK_URL, OPEN_AI_API_KEY }).then(() => {
+	console.log("finished");
+});
